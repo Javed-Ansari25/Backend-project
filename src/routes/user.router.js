d7bab44f4772bbcc6e140e1cefd30c438f9f9f12
Javed Ansari <Javedansari2k09@gmail.com>
2026-01-06 10:02:22 +0530

    refactor user router for consistency and clarity in route naming

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { 
     registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, 
     changeUserDetails, updateAvatar, updateCoverImage , getUserChannelProfile, getWatchHistory
-} from "../controllers/user.controllers.js";
+} 
+from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,10 +26,10 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
-router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/getCurrent-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, changeUserDetails)
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
 
